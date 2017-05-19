f565f51021d45f9457398ab853eb07134e13a95b
Jingshi <js4479@georgetown.edu>
2017-05-19 02:44:08 -0400

    update

--- Part A/partA_sentiment_analysis.py
+++ Part A/partA_sentiment_analysis.py
@@ -114,80 +114,86 @@ class word_assocations:
                 wordcount))       
                 
                 
-def emotions(wordstring):    
+def emotions(years):
+
+    
     associator = word_assocations()
 
-    headers = ['year of debates','negative', 'positive', 'anger', 
+    headers = ['year_of_debates','negative', 'positive', 'anger', 
                'fear', 'anticipation', 'surprise', 'trust', 'sadness', 'joy', 
                'disgust', 'wordcount', 'negative_percent', 'positive_percent',
                'anger_percent', 'fear_percent', 'anticipation_percent',
                'surprise_percent', 'trust_percent', 'sadness_percent', 'joy_percent',
                'disgust_percent']
     
-    # Write headers into output CSV file
+    #Write headers into output CSV file
     with open("sentiments.csv", "w", newline = '', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames = headers)
-        writer.writeheader()    
+        writer.writeheader()
+        
     
-    temp_emotions = associator.count_emotions(wordstring)
-    if temp_emotions[0] > 0:
-        negative_percent    = temp_emotions[0] / temp_emotions[10]
-        positive_percent    = temp_emotions[1] / temp_emotions[10]
-        anger_percent       = temp_emotions[2] / temp_emotions[10]
-        fear_percent        = temp_emotions[3] / temp_emotions[10]
-        anticipation_percent= temp_emotions[4] / temp_emotions[10]
-        surprise_percent    = temp_emotions[5] / temp_emotions[10]
-        trust_percent       = temp_emotions[6] / temp_emotions[10]
-        sadness_percent     = temp_emotions[7] / temp_emotions[10]
-        joy_percent         = temp_emotions[8] / temp_emotions[10]
-        disgust_percent     = temp_emotions[9] / temp_emotions[10]
-        with open("sentiments.csv", "a", newline = '', encoding = 'utf-8') as f:
-            writer = csv.DictWriter(f, fieldnames = headers)
-            writer.writerow({'year of debates': '1963',
-                             'negative': temp_emotions[0],
-                             'positive': temp_emotions[1],
-                             'anger': temp_emotions[2],
-                             'fear': temp_emotions[3],
-                             'anticipation': temp_emotions[4],
-                             'surprise': temp_emotions[5],
-                             'trust': temp_emotions[6],
-                             'sadness': temp_emotions[7],
-                             'joy': temp_emotions[8],
-                             'disgust': temp_emotions[9],
-                             'wordcount': temp_emotions[10], 
-                             'negative_percent': negative_percent,
-                             'positive_percent': positive_percent,
-                             'anger_percent': anger_percent,
-                             'fear_percent': fear_percent,
-                             'anticipation_percent': anticipation_percent,
-                             'surprise_percent': surprise_percent,
-                             'trust_percent': trust_percent,
-                             'sadness_percent': sadness_percent,
-                             'joy_percent': joy_percent,
-                             'disgust_percent': disgust_percent})
+    
+    for year in years:
+        # a string to record all words
+        wordstring =''
+        year = str(year)
+        path = "FlatWorldFiles/"+year+"/Output/*.txt"
+        # read the two files under the folder 1963
+        read_files = glob.glob(path)
+        
+        # add them to the wordstring
+        for f in read_files:
+            with open(f, "rb") as infile:
+                wordstring+=str(infile.read())
+        
+        wordstring=wordstring.lower()
+        temp_emotions = associator.count_emotions(wordstring)
+        if temp_emotions[0] > 0:
+            negative_percent    = temp_emotions[0] / temp_emotions[10]
+            positive_percent    = temp_emotions[1] / temp_emotions[10]
+            anger_percent       = temp_emotions[2] / temp_emotions[10]
+            fear_percent        = temp_emotions[3] / temp_emotions[10]
+            anticipation_percent= temp_emotions[4] / temp_emotions[10]
+            surprise_percent    = temp_emotions[5] / temp_emotions[10]
+            trust_percent       = temp_emotions[6] / temp_emotions[10]
+            sadness_percent     = temp_emotions[7] / temp_emotions[10]
+            joy_percent         = temp_emotions[8] / temp_emotions[10]
+            disgust_percent     = temp_emotions[9] / temp_emotions[10]
+            with open("sentiments.csv", "a", newline = '', encoding = 'utf-8') as f:
+                writer = csv.DictWriter(f, fieldnames = headers)
+                writer.writerow({'year_of_debates': str(year),
+                                 'negative': temp_emotions[0],
+                                 'positive': temp_emotions[1],
+                                 'anger': temp_emotions[2],
+                                 'fear': temp_emotions[3],
+                                 'anticipation': temp_emotions[4],
+                                 'surprise': temp_emotions[5],
+                                 'trust': temp_emotions[6],
+                                 'sadness': temp_emotions[7],
+                                 'joy': temp_emotions[8],
+                                 'disgust': temp_emotions[9],
+                                 'wordcount': temp_emotions[10], 
+                                 'negative_percent': negative_percent,
+                                 'positive_percent': positive_percent,
+                                 'anger_percent': anger_percent,
+                                 'fear_percent': fear_percent,
+                                 'anticipation_percent': anticipation_percent,
+                                 'surprise_percent': surprise_percent,
+                                 'trust_percent': trust_percent,
+                                 'sadness_percent': sadness_percent,
+                                 'joy_percent': joy_percent,
+                                 'disgust_percent': disgust_percent})
 
             
             
 def main():
     
     # set directory
-    os.chdir("/Users/jingshisun/Desktop/KenyaParliamentaryDebates/1963")
-    
-    # a string to record all words
-    wordstring =''
-    
-    # read the two files under the folder 1963
-    read_files = glob.glob("1963/*.txt")
-    
-    # add them to the wordstring
-    for f in read_files:
-        with open(f, "rb") as infile:
-            wordstring+=str(infile.read())
-    
-    wordstring=wordstring.lower()
-       
+    os.chdir("/Users/jingshisun/Desktop/KenyaParliamentaryDebates/sentiment")
+    # specify the years of the files to calculate
+    years = {1963, 1964, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1977, 1978, 1979, 1980, 1981, 1983, 1985, 1987, 1990, 1992, 1996, 1997, 1998, 1999, 2004, 2008}       
     # calculate sentiments based on NRC Emotion and Sentiment Lexicons
-    emotions(wordstring)
+    emotions(years)
     
             
 if __name__ == "__main__":
--- Part A/partA_word_count.py
+++ Part A/partA_word_count.py
@@ -12,15 +12,8 @@ import re
 import csv
 
 
-def main():
+def wordcount(years):
     ###### word count ######
-    
-    # set directory
-    os.chdir("/Users/jingshisun/Desktop/KenyaParliamentaryDebates/1963/1963")
-    
-    # a string to record all words
-    wordstring =''
-    
     # regular expressions for the words to be counted 
     land = re.compile("land")
     infrastructure=re.compile("infrastructure")
@@ -32,49 +25,63 @@ def main():
     economy=re.compile("economy")
     tribe=re.compile("tribe")
     
-    # read the two files under the folder 1963
-    read_files = glob.glob("*.txt")
-    
-    # add them to the wordstring
-    for f in read_files:
-        with open(f, "rb") as infile:
-            wordstring+=str(infile.read())
-            
-    wordstring = wordstring.lower()
-            
-    # using regular expression to find the words and count them
-    land_count = str(len(land.findall(wordstring)))
-    infrastructure_count = str(len(infrastructure.findall(wordstring)))
-    road_count= str(len(road.findall(wordstring)))
-    school_count = str(len(school.findall(wordstring)))
-    hospital_count = str(len(hospital.findall(wordstring)))
-    disease_count = str(len(disease.findall(wordstring)))
-    corruption_count= str(len(corruption.findall(wordstring)))
-    economy_count = str(len(economy.findall(wordstring)))
-    tribe_count=str(len(tribe.findall(wordstring)))
-        
-    headers = ['year of debates','land', 'infrastructure', 'road[s]', 
+    headers = ['year_of_debates','land', 'infrastructure', 'road[s]', 
                'school[s]', 'hospital[s]', 'disease', 'corruption', 'economy', 'tribe']
     
     # Write headers into output CSV file
     with open("wordcount.csv", "w", newline = '', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames = headers)
-        writer.writeheader()    
+        writer.writeheader()
     
-    # Write the word counts into a CSV file
-    with open("wordcount.csv", "a", newline = '', encoding = 'utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames = headers)
-        writer.writerow({'year of debates': '1963',
-                         'land':land_count,
-                         'infrastructure':infrastructure_count,
-                         'road[s]':road_count,
-                         'school[s]':school_count,
-                         'hospital[s]':hospital_count,
-                         'disease':disease_count,
-                         'corruption':corruption_count,
-                         'economy':economy_count,
-                         'tribe':tribe_count})
+    for year in years:
+        # a string to record all words
+        wordstring =''
+        year = str(year)
+        path = "FlatWorldFiles/"+year+"/Output/*.txt"
+        # read the two files under the folder 1963
+        read_files = glob.glob(path)
+        
+        # add them to the wordstring
+        for f in read_files:
+            with open(f, "rb") as infile:
+                wordstring+=str(infile.read())
 
+        wordstring = wordstring.lower()
+                
+        # using regular expression to find the words and count them
+        land_count = str(len(land.findall(wordstring)))
+        infrastructure_count = str(len(infrastructure.findall(wordstring)))
+        road_count= str(len(road.findall(wordstring)))
+        school_count = str(len(school.findall(wordstring)))
+        hospital_count = str(len(hospital.findall(wordstring)))
+        disease_count = str(len(disease.findall(wordstring)))
+        corruption_count= str(len(corruption.findall(wordstring)))
+        economy_count = str(len(economy.findall(wordstring)))
+        tribe_count=str(len(tribe.findall(wordstring)))
+            
+        # Write the word counts into a CSV file
+        with open("wordcount.csv", "a", newline = '', encoding = 'utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames = headers)
+            writer.writerow({'year_of_debates': str(year),
+                             'land':land_count,
+                             'infrastructure':infrastructure_count,
+                             'road[s]':road_count,
+                             'school[s]':school_count,
+                             'hospital[s]':hospital_count,
+                             'disease':disease_count,
+                             'corruption':corruption_count,
+                             'economy':economy_count,
+                             'tribe':tribe_count})
+
+            
+def main():
+    # set directory
+    os.chdir("/Users/jingshisun/Desktop/KenyaParliamentaryDebates/sentiment")
+    # specify the years 
+    years = {1963, 1964, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1977, 1978, 1979, 1980, 1981, 1983, 1985, 1987, 1990, 1992, 1996, 1997, 1998, 1999, 2004, 2008}
+    # calculate word counts
+    wordcount(years)
+    
         
 if __name__ == "__main__":
     
